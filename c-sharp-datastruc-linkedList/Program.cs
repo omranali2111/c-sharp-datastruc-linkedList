e8e864daeb968fdaccfe39e9a436bde051d3089a
omranali2111 <aljabri1234@hotmail.com>
2023-07-10 10:38:22 +0400

    adding two methods (insertAtPosition & find )

--- c-sharp-datastruc-linkedList/Program.cs
+++ c-sharp-datastruc-linkedList/Program.cs
@@ -12,12 +12,20 @@ internal class Program
         linklist.Add(40);
 
         linklist.Print();
-        linklist.Remove(30);
+        //linklist.Remove(30);
 
-        Console.WriteLine("this is after removing data:");
+        //Console.WriteLine("this is after removing data:");
+        // linklist.Print();
+
+       // linklist.addInPosition(30,30);
+        Console.WriteLine("this is after adding data in Position:");
+
+        //  linklist.Print();
+
+        //Console.WriteLine( linklist.Find(30));
+
+        linklist.insertAtPosition(90, 2);
         linklist.Print();
-
-
 
 
     }
--- c-sharp-datastruc-linkedList/linklist.cs
+++ c-sharp-datastruc-linkedList/linklist.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -64,6 +65,89 @@ namespace c_sharp_datastruc_linkedList
            
 
         }
+
+        public void addInPosition(int data,int prevdata)
+        {
+            Node newNode = new Node(data);
+
+            if (head == null)
+            {
+                // If the list is empty, make the new node the head
+                head = newNode;
+            }
+            else
+            {
+                Node currentNode = head;
+
+                while (currentNode != null && currentNode.data != prevdata)
+                {
+                    currentNode = currentNode.Next;
+                }
+
+                if (currentNode != null)
+                {
+                    newNode.Next = currentNode.Next;
+                    currentNode.Next = newNode;
+                }
+                else
+                {
+                    // Node with prevdata not found
+                    Console.WriteLine("Node not found!");
+                   
+                }
+            }
+
+        }
+        public void insertAtPosition(int data, int position)
+        {
+            Node newNode = new Node(data);
+            
+            if (position == 0)
+            {
+                newNode.Next = head;
+                head = newNode;
+            }
+            else
+            {
+                Node currentNode = head;
+                int currentPosition = 0;
+                Node prevNode = null;
+
+                while (currentNode != null && currentPosition < position)
+                {
+                    prevNode = currentNode;
+                    currentNode = currentNode.Next;
+                    currentPosition++;
+                }
+                prevNode.Next = newNode;
+                newNode.Next = currentNode;
+
+            }
+
+        }
+        public int Find(int data)
+        {
+            Node currentNode = head;
+            int position = 0;
+
+            while (currentNode != null)
+            {
+                if (currentNode.data == data)
+                {
+                    Console.Write($"the posposition of node contain data {data} is: ");
+                    return position;
+                }
+
+                currentNode = currentNode.Next;
+                position++;
+            }
+
+            return -1; // Data not found
+        
+
+
+
+    }
         public void Print()
         {
             Node currentNode = head;
